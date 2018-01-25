78b28071a113fd18ea6c5b6b5dbc461761b8fc9d
jiangzikai <3061846560@qq.com>
2018-01-25 22:44:46 +0800

    优化

--- src/main/java/com/kaihang/BiyouApplication.java
+++ src/main/java/com/kaihang/BiyouApplication.java
@@ -5,13 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class BiyouApplication {
-	@RequestMapping("/")
-	String index(){
-		return "hello spring boot";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(BiyouApplication.class, args);
 	}
